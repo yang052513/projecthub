30eb4d03cb21533755912b1784d0addd4b396421
yang052513 <liyang0525.ly@gmail.com>
2020-05-26 23:06:02 -0700

    restyling home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,13 @@ import firebase from 'firebase'
 import Hitokoto from './Home/Hitokoto'
 import Project from './Home/Project'
 import CreateBtn from './Home/CreateBtn'
+import ProjectNav from './Home/ProjectNav'
 
 function Home() {
   return (
     <div className="home-container component-layout">
       <Hitokoto />
+      <ProjectNav />
       <Project />
       <CreateBtn />
     </div>
--- src/components/Home/ProjectNav.js
+++ src/components/Home/ProjectNav.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 function ProjectNav() {
-  return <div className="project-header-container"></div>
+  return (
+    <div className="project-header-container">
+      <h3>All My Projects</h3>
+      <div className="project-header-filter">
+        <select>
+          <option>All My Projects</option>
+          <option>In Progress</option>
+          <option>Completed</option>
+          <option>Planning</option>
+          <option>Dropped</option>
+        </select>
+        <button>Create New Project</button>
+      </div>
+    </div>
+  )
 }
 
 export default ProjectNav
