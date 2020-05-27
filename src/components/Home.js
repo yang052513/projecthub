09f2ece0e05826827a144620cd8f0fc3d0ef3a5e
yang052513 <liyang0525.ly@gmail.com>
2020-05-26 17:21:26 -0700

    hitokoto added to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React from "react"
-import firebase from "firebase"
+import React from 'react'
+import firebase from 'firebase'
+import Hitokoto from './Home/Hitokoto'
+
 function Home() {
   return (
     <div className="component-layout">
-      <p>主界面</p>
-      <button onClick={() => firebase.auth().signOut()}>退出</button>
+      <Hitokoto />
     </div>
   )
 }
---
+++ src/components/Home/Hitokoto.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+function Hitokoto() {
+  const [hitokoto, setHitokoto] = useState({
+    content: '',
+
+    from: '',
+  })
+  const sayUrl = 'https://v1.hitokoto.cn/?c=a&c=b&c=c&c=d&c=k'
+
+  useEffect(() => {
+    fetch(sayUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setHitokoto({
+          content: data.hitokoto,
+
+          from: data.from === null ? '未知' : data.from,
+        })
+      })
+  }, [])
+
+  return (
+    <div className="hitokoto-container">
+      <p className="hitokoto-content">{hitokoto.content}</p>
+      <p className="hitokoto-from">「{hitokoto.from}」</p>
+    </div>
+  )
+}
+
+export default Hitokoto
