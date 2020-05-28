35e93f9735b3c350504f631c6c4147c41293cdc6
yang052513 <liyang0525.ly@gmail.com>
2020-05-28 13:40:00 -0700

    added color style to different status

--- src/components/Home/ProjectCard.js
+++ src/components/Home/ProjectCard.js
@@ -1,21 +1,38 @@
 import React from 'react'
+import { rgbToHex } from '@material-ui/core'
+
+const inProgress = 'rgb(67, 219, 118)'
+const completed = 'rgb(51, 196, 206)'
+const planning = 'rgb(206, 51, 90)'
+const dropped = 'rgb(128, 128, 128)'
 
 function ProjectCard(props) {
+  let statusColor
+  if (props.project.Status === 'In Progress') {
+    statusColor = { backgroundColor: inProgress }
+  } else if (props.project.Status === 'Completed') {
+    statusColor = { backgroundColor: completed }
+  } else if (props.project.Status === 'Planning') {
+    statusColor = { backgroundColor: planning }
+  } else if (props.project.Status === 'Dropped') {
+    statusColor = { backgroundColor: dropped }
+  }
+
   const toolList = props.project.Tools.map((tool) => <li key={tool}>{tool}</li>)
+
   return (
     <div className="project-card-item">
-      <div className="project-line"></div>
+      <div style={statusColor} className="project-line"></div>
       <div className="project-header">
         <p className="project-title">{props.project.Name}</p>
-        <p className="project-status">{props.project.Status}</p>
+        <p style={statusColor} className="project-status">
+          {props.project.Status}
+        </p>
       </div>
       <p className="project-category">{props.project.Category}</p>
       <p className="project-desc">{props.project.Desc}</p>
       <ul className="project-tools">{toolList}</ul>
       <div className="project-contributor">
-        <img src="/images/user.jpg" alt="contributor" />
-        <img src="/images/user.jpg" alt="contributor" />
-        <img src="/images/user.jpg" alt="contributor" />
         <img src="/images/user.jpg" alt="contributor" />
       </div>
       <div className="project-footer">
