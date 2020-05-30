58c64d4c64fa257cc72a5fd2543f3b3d6351c187
yang052513 <liyang0525.ly@gmail.com>
2020-05-30 12:09:48 -0700

    toggle particle off now

--- src/components/Common/Logout.js
+++ src/components/Common/Logout.js
@@ -15,24 +15,24 @@ function Logout() {
           setUserName(doc.data().Name)
         })
     })
-    setTimeout(() => {
-      firebase
-        .auth()
-        .signOut()
-        .then(
-          function () {
-            console.log('退出成功 bye bye')
-          },
-          function (error) {
-            console.error('退出也能有错误？那可能是你的网的问题', error)
-          }
-        )
-    }, 3000)
+    // setTimeout(() => {
+    //   firebase
+    //     .auth()
+    //     .signOut()
+    //     .then(
+    //       function () {
+    //         console.log('退出成功 bye bye')
+    //       },
+    //       function (error) {
+    //         console.error('退出也能有错误？那可能是你的网的问题', error)
+    //       }
+    //     )
+    // }, 3000)
   }, [])
 
   return (
     <div className="logout-container">
-      <Particle />
+      {/* <Particle /> */}
       <p>Take care {userName}.</p>
     </div>
   )
