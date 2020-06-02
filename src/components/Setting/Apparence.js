f20df3409fd97910d0b6357863bc3b77b586bd4f
yang052513 <liyang0525.ly@gmail.com>
2020-06-01 19:23:04 -0700

    added opacity slider

--- src/components/Setting/Apparence.js
+++ src/components/Setting/Apparence.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Theme from './Apparence/Theme'
 import Background from './Apparence/Background'
+import Opacity from './Apparence/Opacity'
+
 export default function Apparence() {
   return (
     <div className="setting-content-container">
       <h2>Apparence</h2>
       <Theme />
       <Background />
-      <h3>Opacity</h3>
+      <Opacity />
     </div>
   )
 }
---
+++ src/components/Setting/Apparence/Opacity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import OpacitySlider from './OpacitySlider'
+
+export default function Opacity() {
+  return (
+    <div>
+      <h3>Opacity</h3>
+      <OpacitySlider />
+    </div>
+  )
+}
