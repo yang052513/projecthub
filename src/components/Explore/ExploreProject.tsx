125a53605061e84ab601ca461b41a1609d17adad
yang052513 <liyang0525.ly@gmail.com>
2020-07-22 15:48:14 -0700

    explore fetch project added

--- src/components/Explore/ExploreProject.tsx
+++ src/components/Explore/ExploreProject.tsx
@@ -5,48 +5,27 @@ interface Props {
 }
 
 export const ExploreProject: React.FC<Props> = ({ projectData }) => {
+  const exploreProjectList = projectData.map((item: any) => (
+    <div key={item.Key} className="project-card-item">
+      <div className="project-header">
+        <p className="project-title">{item.projectData.Name}</p>
+      </div>
+      <p className="project-category">{item.projectData.Category}</p>
+      <p className="project-desc">{item.projectData.Desc}</p>
+
+      <ul className="project-tools">
+        {item.projectData.Tools.map((tool: any) => (
+          <li key={tool}>{tool}</li>
+        ))}
+      </ul>
+      <p className="project-category">last updated on Jul 22, 2020 13:20</p>
+      <img className="project-author-avatar" src="/images/user.jpg" alt="" />
+    </div>
+  ))
   return (
     <div className="explore-project-list-container">
       <h2>Explore Projects</h2>
-      <div className="project-card-item">
-        <div className="project-header">
-          <p className="project-title">Course Flex</p>
-        </div>
-        <p className="project-category">Web App</p>
-        <p className="project-desc">
-          🗓 CC college course schedule generator with timetable preview
-        </p>
-
-        <ul className="project-tools">
-          <li>hexo</li>
-          <li>react</li>
-          <li>Typescript</li>
-          <li>mongoDB</li>
-        </ul>
-
-        <p className="project-category">last updated on Jul 22, 2020 13:20</p>
-        <img className="project-author-avatar" src="/images/user.jpg" alt="" />
-      </div>
-
-      <div className="project-card-item">
-        <div className="project-header">
-          <p className="project-title">Course Flex</p>
-        </div>
-        <p className="project-category">Web App</p>
-        <p className="project-desc">
-          🗓 CC college course schedule generator with timetable preview
-        </p>
-
-        <ul className="project-tools">
-          <li>hexo</li>
-          <li>react</li>
-          <li>Typescript</li>
-          <li>mongoDB</li>
-        </ul>
-
-        <p className="project-category">last updated on Jul 22, 2020 13:20</p>
-        <img className="project-author-avatar" src="/images/user.jpg" alt="" />
-      </div>
+      {exploreProjectList}
     </div>
   )
 }
