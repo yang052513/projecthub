a40027a628c5e334ac99d8833cd50ee541e2dfb8
yang052513 <liyang0525.ly@gmail.com>
2020-07-22 13:13:44 -0700

    explore component static contents

--- src/components/Explore/ExploreProject.tsx
+++ src/components/Explore/ExploreProject.tsx
@@ -3,6 +3,27 @@ import React from 'react'
 export const ExploreProject: React.FC = () => {
   return (
     <div className="explore-project-list-container">
+      <h2>Explore Projects</h2>
+      <div className="project-card-item">
+        <div className="project-header">
+          <p className="project-title">Course Flex</p>
+        </div>
+        <p className="project-category">Web App</p>
+        <p className="project-desc">
+          🗓 CC college course schedule generator with timetable preview
+        </p>
+
+        <ul className="project-tools">
+          <li>hexo</li>
+          <li>react</li>
+          <li>Typescript</li>
+          <li>mongoDB</li>
+        </ul>
+
+        <p className="project-category">last updated on Jul 22, 2020 13:20</p>
+        <img className="project-author-avatar" src="/images/user.jpg" alt="" />
+      </div>
+
       <div className="project-card-item">
         <div className="project-header">
           <p className="project-title">Course Flex</p>
--- src/components/Explore/ExploreTrending.tsx
+++ src/components/Explore/ExploreTrending.tsx
@@ -4,8 +4,13 @@ export const ExploreTrending: React.FC = () => {
   return (
     <div className="explore-trending-container">
       <h3>Trending</h3>
-      <img src="./images/user.jpg" alt="" width="80px" height="80px" />
-      <p>Nathan Lee</p>
+      <div className="explore-trending-item">
+        <div>
+          <img src="./images/user.jpg" alt="" width="80px" height="80px" />
+          <p>Nathan Lee</p>
+        </div>
+        <button>Follow</button>
+      </div>
     </div>
   )
 }
