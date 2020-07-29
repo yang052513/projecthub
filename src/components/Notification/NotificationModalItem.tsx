5fa3c18905e8a3aca785e8045562b0f6b0d64db3
yang052513 <liyang0525.ly@gmail.com>
2020-07-29 15:29:47 -0700

    added animation for notification toggle

--- src/components/Notification/NotificationModalItem.tsx
+++ src/components/Notification/NotificationModalItem.tsx
@@ -13,7 +13,9 @@ export const NotificationModalItem: React.FC<Props> = ({
       <div className="notification-item">
         <img src={notificationData.Avatar} alt="" />
         <div className="notification-item-text">
-          <p>New {notificationData.Category}</p>
+          <p className="notification-item-category">
+            New {notificationData.Category}
+          </p>
 
           <p className="notification-item-msg">{notificationData.Message}</p>
           <p className="notification-item-time">{notificationData.Date}</p>
