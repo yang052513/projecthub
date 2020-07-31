7ebb37cc3a9f6673b30de116d4bc26d2a0e1a83f
yang052513 <liyang0525.ly@gmail.com>
2020-07-30 18:25:23 -0700

    sender receiver styling float done

--- src/components/Messenger/MessengerChat.tsx
+++ src/components/Messenger/MessengerChat.tsx
@@ -7,10 +7,26 @@ export const MessengerChat: React.FC = () => {
         <p>Nathan Lee</p>
       </div>
       <div className="messenger-chat-display-container">
-        <p>Hisdasd adasdloren Lorem, ipsum dolor.</p>
-        <p>Hisdasd adasdloren Lorem, ipsum dolor.</p>
-        <p>Hisdasd adasdloren Lorem, ipsum dolor.</p>
-        <p>Hisdasd adasdloren Lorem, ipsum dolor.</p>
+        <div className="messenger-chat-item chat-receiver">
+          <img src="./images/user.jpg" alt="" />
+          <p>
+            We're headed to Tahoe this weekend. Something warm would be nice...
+          </p>
+        </div>
+
+        <div className="messenger-chat-item chat-sender">
+          <p>
+            We're headed to Tahoe this weekend. Something warm would be nice...
+          </p>
+          <img src="./images/user.jpg" alt="" />
+        </div>
+
+        <div className="messenger-chat-item chat-sender">
+          <p>
+            We're headed to Tahoe this weekend. Something warm would be nice...
+          </p>
+          <img src="./images/user.jpg" alt="" />
+        </div>
       </div>
 
       <div className="messenger-chat-box-container">Text area</div>
