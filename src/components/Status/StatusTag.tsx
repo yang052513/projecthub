cfc0bdb88b20541497771ca418ba1ec821bae396
yang052513 <liyang0525.ly@gmail.com>
2020-08-04 23:15:31 -0700

    added tool chart for project tags

--- src/components/Status/StatusTag.tsx
+++ src/components/Status/StatusTag.tsx
@@ -1,4 +1,52 @@
 import React from 'react'
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts'
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+  },
+]
 
 interface Props {
   tagSort: any
@@ -14,8 +62,26 @@ export const StatusTag: React.FC<Props> = ({ tagSort }) => {
   return (
     <div className="status-card-item-wrap">
       <h3>Most Used Technology</h3>
+
       <div className="status-card-container">
-        <ul>{topThreeTag}</ul>
+        <BarChart
+          width={500}
+          height={300}
+          data={tagSort}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="cnt" fill="#8884d8" />
+        </BarChart>
       </div>
     </div>
   )
