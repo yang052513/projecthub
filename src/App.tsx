f8ab8dc5362d5be315b941c5c2bc182bc4bdfa79
yang052513 <liyang0525.ly@gmail.com>
2020-08-18 12:35:47 -0700

    added ProfileContext

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ import { Navigator } from './router/Navigator'
 import { SideNavBar } from './components/navigation/SideNavBar'
 import { NavigationHeader } from './components/navigation/NavigationHeader'
 
+import { ProfileContext } from './context/ProfileContext'
+import { useFetchProfile } from './hooks/useFetchProfile'
+
 export default function App() {
   const user: firebase.User | null = firebase.auth().currentUser
-
+  const userProfile = useFetchProfile(user!.uid)
   //全局样式化
   const [theme, setTheme] = useState('#0e5dd3')
   const [options, setOptions] = useState('Color')
@@ -230,50 +233,52 @@ export default function App() {
   }
 
   return (
-    <div>
-      {/* Global CSS styles */}
-      {demo.backgroundColor ? (
+    <ProfileContext.Provider value={userProfile}>
+      <div>
+        {/* Global CSS styles */}
+        {demo.backgroundColor ? (
+          <div
+            style={{
+              backgroundColor: demo.backgroundRef,
+              transition: 'all 2s',
+            }}
+            className="background"
+          ></div>
+        ) : (
+          <div
+            style={{ backgroundImage: `url(${demo.backgroundRef})` }}
+            className="background-img"
+          ></div>
+        )}
+        {/* Overlay for backgroud image to control opacity */}
         <div
-          style={{
-            backgroundColor: demo.backgroundRef,
-            transition: 'all 2s',
-          }}
-          className="background"
+          className="overlay"
+          style={{ opacity: opacity.background / 100 }}
         ></div>
-      ) : (
-        <div
-          style={{ backgroundImage: `url(${demo.backgroundRef})` }}
-          className="background-img"
-        ></div>
-      )}
-      {/* Overlay for backgroud image to control opacity */}
-      <div
-        className="overlay"
-        style={{ opacity: opacity.background / 100 }}
-      ></div>
 
-      {/* Content container */}
-      <div className="content-container">
-        <img className="logo" src="/images/logo.png" alt="" />
+        {/* Content container */}
+        <div className="content-container">
+          <img className="logo" src="/images/logo.png" alt="" />
 
-        <SideNavBar opacity={opacity} theme={theme} />
-        <NavigationHeader opacity={opacity} avatar={avatar} />
+          <SideNavBar opacity={opacity} theme={theme} />
+          <NavigationHeader opacity={opacity} avatar={avatar} />
 
-        {/* Router */}
-        <Navigator
-          avatar={avatar}
-          profile={profile}
-          demo={demo}
-          options={options}
-          customBg={customBg}
-          opacity={opacity}
-          handleColor={handleColor}
-          handleOpacity={handleOpacity}
-          handleOptions={handleOptions}
-          handleSwitch={handleSwitch}
-          handleTheme={handleTheme}
-        />
+          {/* Router */}
+          <Navigator
+            avatar={avatar}
+            profile={profile}
+            demo={demo}
+            options={options}
+            customBg={customBg}
+            opacity={opacity}
+            handleColor={handleColor}
+            handleOpacity={handleOpacity}
+            handleOptions={handleOptions}
+            handleSwitch={handleSwitch}
+            handleTheme={handleTheme}
+          />
+        </div>
       </div>
-    </div>
+    </ProfileContext.Provider>
   )
 }
